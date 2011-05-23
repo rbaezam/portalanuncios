659dd2211e5a5bf91339875bb85e8613b22aa44f
Rodolfo Baeza <rbaezam@hotmail.com>
2011-05-22 22:47:53 -0500

    Proyecto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portalanuncios::Application.config.secret_token = 'b257be2ed3b7791049e57256d05e0782e1d98a13b511cf59623f908fdb607f85724fed5c50a6b39cbde7a9a5db9c9691fcbafb850e8a39cd1822c846a22c8761'
